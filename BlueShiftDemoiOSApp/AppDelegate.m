c88bea4c6d0a9e2bdd7e1948e50db87079dfca8c
shahas <shahas@bullfin.ch>
2016-10-17 17:29:12 +0530

    carosuel image deep linking

--- BlueShiftDemoiOSApp/AppDelegate.m
+++ BlueShiftDemoiOSApp/AppDelegate.m
@@ -87,6 +87,10 @@
 }
 
 
+- (void)handleCarouselPushForCategory:(NSString *)categoryName clickedWithDetails:(NSDictionary *)detalis andDeepLinkURL:(NSString *)url {
+    NSLog(@"url is %@", url);
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
